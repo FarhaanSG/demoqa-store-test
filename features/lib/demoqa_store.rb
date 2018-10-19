18b40b0a618aa3cf6943b7a6314bb8c955149d34
christianmilson <cbingle-williams@spartaglobal.com>
2018-10-19 15:35:03 +0100

    sign up tests complete

--- features/lib/demoqa_store.rb
+++ features/lib/demoqa_store.rb
@@ -1,9 +1,14 @@
 require_relative 'pages/login_service.rb'
+require_relative 'pages/sign_up_service.rb'
 
 module DemoQAStore
 
     def login_service
         DemoQALogin.new
+    end
+
+    def sign_up_service
+        DemoQASignUp.new
     end
 
 
