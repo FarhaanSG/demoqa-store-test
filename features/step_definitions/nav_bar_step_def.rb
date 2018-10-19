c21af56c1258d6dbc3957d39c1734e44a110b418
aymanthefirst <AHarake@spartaglobal.com>
2018-10-19 14:29:25 +0100

    nav bar testing done

--- features/step_definitions/nav_bar_step_def.rb
+++ features/step_definitions/nav_bar_step_def.rb
@@ -3,21 +3,61 @@ Given("I am on the home page") do
 end
 
 When("I hover over the Product Category link") do
-
+  home_page.hover_over_product_category
 end
 
 Then("a drop down box containing more options should appear") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(home_page.does_drop_down_appear?).to be true
 end
 
 Then("the links should only contain letters") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(home_page.do_links_contain_only_letters?).to be true
 end
 
-Given("I am on the any page") do
-  pending # Write code here that turns the phrase above into concrete actions
+Then("the Product Category link should lead to the Product Category page") do
+  home_page.click_product_category_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/")
 end
 
-Then("all links should lead to a certain page or location") do
-  pending # Write code here that turns the phrase above into concrete actions
+And("the Accessories link should lead to the Accessories page") do
+  home_page.hover_over_product_category
+  home_page.click_accessories_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/accessories/")
+end
+
+And("the iMacs link should lead to the iMacs page") do
+  home_page.hover_over_product_category
+  home_page.click_iMacs_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/imacs/")
+end
+
+Then("the iPads link should lead to the iPads page") do
+  home_page.hover_over_product_category
+  home_page.click_ipads_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/ipads/")
+end
+
+
+And("the iPhones link should lead to the iPhones page") do
+  home_page.hover_over_product_category
+  home_page.click_iphones_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/iphones/")
+end
+
+And("the iPods link should lead to the iPods page") do
+  home_page.hover_over_product_category
+  home_page.click_ipods_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/ipods/")
+end
+
+And("the MacBooks link should lead to the MacBooks page") do
+  home_page.hover_over_product_category
+  home_page.click_macbooks_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/macbooks/")
+end
+
+And("the All Product link should lead to the All Product page") do
+  home_page.hover_over_product_category
+  home_page.click_all_product_link
+  expect(home_page.current_url).to eq("http://store.demoqa.com/products-page/product-category/")
 end
