72bb6026c931364b912e18ae50f3f22435f578b5
aymanthefirst <AHarake@spartaglobal.com>
2018-10-21 19:28:24 +0100

    working on search box

---
+++ features/step_definitions/search_step_def.rb
@@ -0,0 +1,22 @@
+When("I type valid product details in the search bar") do
+puts "testing start"
+demoqa_homepage.visit_homepage
+
+ search.fill_in_search_box("hello")
+
+end
+
+
+Then("I should get the valid results") do
+  #   puts "testing start"
+  #   puts find("#content").find(".product_image")
+end
+
+When("I type invalid product details in the search bar") do
+
+end
+
+
+Then("I should receive an error for not finding the product") do
+
+end
